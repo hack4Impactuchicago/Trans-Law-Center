f4426d4092a64b60a76c40e55d7526b9a925bf87
Laya Gollapudi <layagollapudi@Layas-MacBook-Pro.local>
2019-03-02 14:44:46 -0600

    more dashboard and login changes

--- main.go
+++ main.go
@@ -15,17 +15,24 @@ func main() {
     ////FOR Login Testing
     src.CreateUser("testuser", "testpsw", 2)
 
+    //// For DB TESTING
     err := src.LoadPresetDBContent("formdb.db")
     if err != nil {
       log.Fatal("Loading Preset...: ", err)
     }
 
+    //VIEWS
+
     http.HandleFunc("/home/", src.ViewHandler)
     http.HandleFunc("/results/", src.ResultsHandler)
     http.HandleFunc("/admin/", src.AdminHandler)
 
+    //CONTENT
+
     http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
     http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
+
+    //SERVER
 
     err = http.ListenAndServe(":8080", nil)
     if err != nil {
